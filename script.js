f72ebaac31b08634d00e082aee1cf2afadfdf03f
Md. Mosfikur Rahman <mdmosfikurrahman.cse@gmail.com>
2023-11-13 13:20:56 +0600

    updated publication (just need to add doi link now)

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        
-        // scroll-up button show/hide script
+
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -15,25 +13,20 @@ $(document).ready(function(){
         }
     });
 
-    // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
-    // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["Software Engineer", "Backend Developer", "Researcher", "Data Analyst"],
         typeSpeed: 100,
@@ -48,7 +41,6 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
@@ -84,7 +76,7 @@ function openModal(reference) {
         <p>${reference.company}</p>
         <br>
         <p>Contact: ${reference.contact}</p>
-        <p>Email: <a href="mailto:${reference.email}" style="color: #fff;">${reference.email}</a></p>
+        <p>Email: <a href="mailto:${reference.email}" style="color: #111;">${reference.email}</a></p>
     `;
 
     modal.style.display = "block";
